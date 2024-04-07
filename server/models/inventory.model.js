c86fe3f8a36072ff405a3ec4ee0c569a51658ce0
Kavindu Dasanayaka <Kavindudasanayaka23@gmail.com>
2024-04-07 10:24:53 +0530

    Create inventory route model and controller

---
+++ server/models/inventory.model.js
@@ -0,0 +1,44 @@
+import mongoose from 'mongoose';
+
+const inventorySchema = new mongoose.Schema(
+  {
+
+    medicineName: { 
+      type: String,
+      required: true,
+    },
+
+    unitPrice: {
+      type: Number,
+      required: true,
+    },
+    quantity: {
+      type: Number,
+      required: true,
+    },
+    expirationDate: {
+        type:Date,
+        required: true,
+    },
+
+    manufacturesDate: {
+        type:Date,
+        required: true,
+    },
+
+    storageCondition: {
+        type:String,
+        required:true,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+
+  },
+  { timestamps: true }
+);
+
+const Inventory = mongoose.model('Listing', inventorySchema);  //Create model
+
+export default Inventory;
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import dbCon from './utils/db.js'
 import cors from 'cors'
 import promotionRouter from './routes/promotion.route.js'
 import employeeRouter from './routes/employee.route.js'
+import inventoryRouter from './routes/inventory.route.js'
 
 const app = express()
 dbCon()
@@ -12,6 +13,7 @@ app.use(cors())
 
 app.use('/api', promotionRouter)
 app.use('/api/employee', employeeRouter)
+app.use('/api/inventory', inventoryRouter)
 
 app.listen(3000, () => {
     console.log('Server is listening on port 3000');
