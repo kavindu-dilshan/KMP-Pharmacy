05ced5789d848374a79136f3e8bd31971021eab5
Kavindu Dilshan <kavindudilshan84@gmail.com>
2024-04-07 12:49:36 +0530

    create feedback model and API route

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ import promotionRouter from './routes/promotion.route.js'
 import employeeRouter from './routes/employee.route.js'
 import supplierRouter from './routes/supplier.route.js'
 import inventoryRouter from './routes/inventory.route.js'
+import feedbackRouter from './routes/feedback.route.js'
 
 const app = express()
 dbCon()
@@ -16,6 +17,7 @@ app.use('/api', promotionRouter)
 app.use('/api/employee', employeeRouter)
 app.use('/api/supplier', supplierRouter)
 app.use('/api/inventory', inventoryRouter)
+app.use('/api/feedback', feedbackRouter)
 
 app.listen(3000, () => {
     console.log('Server is listening on port 3000');
