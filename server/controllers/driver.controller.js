e461c3b1ec7d6d78d83c75ebcfaefd770a48934b
Dilmani Kiriella <it22324856@my.sliit.lk>
2024-05-08 12:44:54 +0530

    Modified Sign in

--- server/controllers/driver.controller.js
+++ server/controllers/driver.controller.js
@@ -117,8 +117,9 @@ const signIn = async (req, res, next) =>   {
   try {
     const validUser = await Driver.findOne({ driverId });
     if (!validUser) return next(errorHandler(404, 'Driver not found!'));
-    const validPassword = validUser.password;
-    if (!validPassword) return next(errorHandler(401, 'Wrong credentials!'));
+    if (validUser.password !== password) {
+      return next(errorHandler(401, 'Wrong credentials!'));
+    }
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
     const { password: pass, ...rest } = validUser._doc;
     res
